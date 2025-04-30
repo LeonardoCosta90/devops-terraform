32047ccb238e291919668562d27499fad4d4cc21
Leonardo <leonardo.everton@inatel.br>
2025-04-29 21:41:14 -0300

    refactor: migrate S3 bucket configuration to module and remove unused files

---
+++ modules/s3/outputs.tf
@@ -0,0 +1,11 @@
+output "bucket_domain_name" {
+  value       = aws_s3_bucket.bucket.bucket_domain_name
+  sensitive   = false
+  description = "Nome de domínio do bucket S3"
+}
+
+output "bucket_id" {
+  value       = aws_s3_bucket.bucket.id
+  sensitive   = false
+  description = "ID do bucket S3"
+}
