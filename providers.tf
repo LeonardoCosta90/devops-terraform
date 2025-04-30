a5c0dc7c902c043e5f070f21739f818bb4aba4e4
Leonardo <leonardo.everton@inatel.br>
2025-04-30 19:42:38 -0300

    feat: add S3 backend configuration and state bucket variable

--- providers.tf
+++ providers.tf
@@ -5,5 +5,23 @@ terraform {
       version = "5.96.0"
     }
   }
+  backend "s3" {
+    bucket  = "backup-state-bucket-tf"
+    region  = "us-east-2"
+    key     = "terraform.tfstate"
+    encrypt = true  
+  }
 }
-provider "aws" {}
+
+
+provider "aws" {
+  region  = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "state_bucket" {
+  type        = string
+  default     = "backup-state-bucket-tf"
+  description = "Bucket com o estado"
+}
