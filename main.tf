f99354e5be9d29cad9ee130b107d6879b08f9839
Leonardo <leonardo.everton@inatel.br>
2025-04-30 14:27:04 -0300

    feat: add SQS module and integrate CloudFront and S3 modules with data sources and outputs

--- main.tf
+++ main.tf
@@ -19,14 +19,3 @@ module "cloudfront" {
     module.s3
   ]
 }
-
-module "sqs" {
-  source  = "terraform-aws-modules/sqs/aws"
-  name = "queue-sqs"
-
-  create_dlq = true
-
-  tags = {
-    Iac = "true"
-  }
-}
